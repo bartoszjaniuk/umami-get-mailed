d04c7322e1764cc3cfc4e77b1c32173e3280b160
Bartosz <bartosz.janiuk@tsh.io>
2022-11-05 17:05:36 +0100

    add delete console.log

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -2,7 +2,6 @@ const AppError = require("../utils/appError");
 const handleSendEmail = require("../utils/handleSendEmail");
 
 exports.sendEmail = (req, res, next) => {
-  console.log(req.body, "req.body");
   try {
     handleSendEmail({
       email: req.body.email,
@@ -15,7 +14,11 @@ exports.sendEmail = (req, res, next) => {
       message: "Mail został wysłany",
     });
   } catch (err) {
-    console.log({ err });
+    // res.status(400).json({
+    //   status: "fail",
+    //   message: "Nie udało się przesłać wiadomości. Spróbuj ponownie później.",
+    // });
+
     return next(
       new AppError(
         "Podczas wysyłania wiadomości wystąpił błąd. Spróbuj ponownie później!"
