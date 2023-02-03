3489b3376fe3969728864b867011919bb3b9fd75
Bartosz <bartosz.janiuk@tsh.io>
2023-02-03 09:06:57 +0100

    add scheduled pings

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const emailRouter = require("./routes/emailRoutes");
 const AppError = require("./utils/appError");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const schedule = require("node-schedule");
+const ping = require("ping");
 
 const app = express();
 
@@ -12,6 +14,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api/v1/email", emailRouter);
+
+const job = schedule.scheduleJob("*/1 * * * *", async function (fireDate) {
+  const result = await ping.promise.probe("www.dietetyk-umami.pl", {
+    timeout: 10,
+    extra: ["-i", "2"],
+  });
+
+  console.log(result);
+});
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Cant find ${req.originalUrl} on this server.`, 404));
